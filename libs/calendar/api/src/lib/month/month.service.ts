5e4f1d80656d38df04cdb4a6af449d3f35560709
alex.toplijski <alex.toplijski@gmail.com>
2021-03-06 18:28:17 +0200

    Introduce month.service

---
+++ libs/calendar/api/src/lib/month/month.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import {YearService} from "../year/year.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MonthService {
+  private readonly firstDayNumber = 1;
+
+  constructor(private yearService: YearService) { }
+
+  getFirstDay(): number {
+    return  this.firstDayNumber;
+  }
+
+  getLastDay(year: number, month: number): number {
+    return  this.countDays(year, month);
+  }
+
+  countDays(year: number, month: number): number {
+    const maxDaysInMonth: number[] = [31, this.yearService.isLeap(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    return maxDaysInMonth[month];
+  }
+
+
+
+
+}
