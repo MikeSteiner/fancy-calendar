068037528c955abc0e6cecc90fd1ed0b866e4e13
alex.toplijski <alex.toplijski@gmail.com>
2021-03-06 23:32:38 +0200

    Small changes in the week.service and dashboard-view component

--- libs/calendar/features/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
+++ libs/calendar/features/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
@@ -1,41 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
+
 import {MeetingsService} from "../../../../../api/src/lib/meetings/meetings.service";
 import {WeekService} from "../../../../../api/src/lib/week/week.service";
 import {MonthService} from "../../../../../api/src/lib/month/month.service";
 import {CalendarGeneratorService} from "../../../../../api/src/lib/calendar-generator/calendar-generator.service";
+import {Week, WeekUTCDay} from "../../../../../api/src/lib/types/types";
 
 @Component({
-  selector: 'fancy-calendar-view',
+  selector: 'fancy-dashboard-view',
   templateUrl: './dashboard-view.component.html',
   styleUrls: ['./dashboard-view.component.scss']
 })
 export class DashboardViewComponent implements OnInit {
 
-  current: Date = new Date();
-  prev: Date = this.addDays(this.current, -1);
-  next: Date = this.addDays(this.current, 1) ;
+  current = new Date();
+  prev = this.calendarGeneratorService.addDays(this.current, -31);
+  next = this.calendarGeneratorService.addDays(this.current, 31) ;
 
   meetings = this.meetingsService.getAll();
-  weekNumber = this.weekService.getNumber(this.current);
-  weekNumbersInMonth = this.calendarGeneratorService.getWeekNumbersInMonth(this.current);
-  weeksInMonth = this.calendarGeneratorService.getWeeksInMonth(this.current.getFullYear(), this.current.getMonth());
 
+  // weekNumber = this.weekService.getNumber(this.current);
+  // weekNumbersInMonth = this.calendarGeneratorService.getWeekNumbersInMonth(this.current);
+  // weeksInMonth = this.calendarGeneratorService.getWeeksInMonth(this.current.getFullYear(), this.current.getMonth());
+  // weeksInMonth: Week[];
+
+  // weeksInMonth: Week[] = this.calendarGeneratorService.getWeeksInMonth(this.current.getFullYear(), this.current.getMonth());
+  weeksInMonth: Week[] = this.calendarGeneratorService.getWeeksInMonth(this.next.getFullYear(), this.next.getMonth());
+
+  daysOfWeek = Object.keys(WeekUTCDay).filter((e) => isNaN(Number(e)));
 
   constructor(
     private meetingsService: MeetingsService,
     private weekService: WeekService,
     private monthService: MonthService,
     private calendarGeneratorService: CalendarGeneratorService
-  ) { }
+  ) {
+    console.log(this.next)
+  }
 
   ngOnInit(): void {
     // this.meetings = this.meetingsService.getAll();
   }
 
-  private addDays(date, days): Date {
-    let result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
-  }
 }
