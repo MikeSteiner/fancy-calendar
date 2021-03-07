20e40136a226b6cc5a1b2c7b47fe01d0ef7edce9
alex.toplijski <alex.toplijski@gmail.com>
2021-03-07 09:59:28 +0200

    Add BehaviorSubject for the http meetings response; Code cleanup and duplicates removal; Add month prev/next navigation; Add meetings mapper methods and render the meetings on the calendar.

--- libs/calendar/features/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
+++ libs/calendar/features/dashboard/src/lib/dashboard-view/dashboard-view.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 
 import {MeetingsService} from "../../../../../api/src/lib/meetings/meetings.service";
 import {WeekService} from "../../../../../api/src/lib/week/week.service";
 import {MonthService} from "../../../../../api/src/lib/month/month.service";
 import {CalendarGeneratorService} from "../../../../../api/src/lib/calendar-generator/calendar-generator.service";
-import {Week, WeekUTCDay} from "../../../../../api/src/lib/types/types";
+import {Meeting, Week, WeekUTCDay} from "../../../../../api/src/lib/types/types";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'fancy-dashboard-view',
@@ -13,20 +14,26 @@ import {Week, WeekUTCDay} from "../../../../../api/src/lib/types/types";
   styleUrls: ['./dashboard-view.component.scss']
 })
 export class DashboardViewComponent implements OnInit {
+  private _meetings$: BehaviorSubject<Meeting[]> = new BehaviorSubject<Meeting[]>([]);
 
-  current = new Date();
-  prev = this.calendarGeneratorService.addDays(this.current, -31);
-  next = this.calendarGeneratorService.addDays(this.current, 31) ;
+  private get year(): number {
+    return this.displayDate.getFullYear();
+  }
 
-  meetings = this.meetingsService.getAll();
+  private get month(): number {
+    return this.displayDate.getMonth();
+  }
 
-  // weekNumber = this.weekService.getNumber(this.current);
-  // weekNumbersInMonth = this.calendarGeneratorService.getWeekNumbersInMonth(this.current);
-  // weeksInMonth = this.calendarGeneratorService.getWeeksInMonth(this.current.getFullYear(), this.current.getMonth());
-  // weeksInMonth: Week[];
+  private get daysInMonthCount(): number {
+    return this.monthService.countDays(this.year, this.month);
+  }
 
-  // weeksInMonth: Week[] = this.calendarGeneratorService.getWeeksInMonth(this.current.getFullYear(), this.current.getMonth());
-  weeksInMonth: Week[] = this.calendarGeneratorService.getWeeksInMonth(this.next.getFullYear(), this.next.getMonth());
+  get meetings$(): Observable<Meeting[]> {
+    return this._meetings$.asObservable()
+  };
+
+  displayDate = new Date();
+  weeksInMonth: Week[];
 
   daysOfWeek = Object.keys(WeekUTCDay).filter((e) => isNaN(Number(e)));
 
@@ -35,12 +42,50 @@ export class DashboardViewComponent implements OnInit {
     private weekService: WeekService,
     private monthService: MonthService,
     private calendarGeneratorService: CalendarGeneratorService
-  ) {
-    console.log(this.next)
-  }
+  ) { }
 
   ngOnInit(): void {
-    // this.meetings = this.meetingsService.getAll();
+    this.loadMeetingsPerMonth();
+    this.loadWeeksPerMonth();
   }
 
+  onPrevMonthClick(): void {
+    this.displayDate = this.calendarGeneratorService.addDays(this.displayDate, (-1) * this.daysInMonthCount);
+    this.loadMeetingsPerMonth();
+    this.loadWeeksPerMonth();
+  }
+
+  onNextMonthClick(): void {
+    this.displayDate = this.calendarGeneratorService.addDays(this.displayDate, this.daysInMonthCount);
+    this.loadMeetingsPerMonth();
+    this.loadWeeksPerMonth();
+  }
+
+  onTodayClick(): void {
+    this.displayDate = new Date();
+    this.loadMeetingsPerMonth();
+    this.loadWeeksPerMonth();
+  }
+
+  loadMeetingsPerDay(day: number): Observable<Meeting[]> {
+    return this.meetings$.pipe(
+      map((meetings: Meeting[]) => meetings.filter((meeting) => {
+        const isInCurrentDay: boolean = meeting.start.getDate() === day;
+        return isInCurrentDay;
+      }))
+    )
+  }
+
+  private loadMeetingsPerMonth(): void {
+    this.meetingsService.getByMonth(this.year, this.month).subscribe(
+      (meetings) => {
+        this._meetings$.next(meetings);
+      },
+      (error) => { return console.log("Error retrieving Todos"); }
+      );
+  }
+
+  private loadWeeksPerMonth(): void {
+    this.weeksInMonth = this.calendarGeneratorService.getWeeksInMonth(this.year, this.month);
+  }
 }
